f0c7755997a3e4a61358d3c739189e58dd3442fa
alihaiderbanoqabil <alihaider.banoqabil2.0@gmail.com>
2025-12-28 11:32:37 +0500

    done with class 2 of js

---
+++ class14/script.js
@@ -0,0 +1,118 @@
+// "use strict";
+console.log("Hello, World!");
+
+// var a = 10 + 20 + 30 + 40 + 50 + 60 + 70 + 80 + 90 + 100;
+// // here i am storing my name in a variable
+// var a = "Ali Haider";
+
+var _name = "Ali Haider";
+var $name = "Ali Haider";
+// var name = "Ali Haider";
+var na1me2 = "Ali Haider";
+// var @name = "Ali Haider";
+// var !name = "Ali Haider";
+// var ^name = "Ali Haider";
+// var name* = "Ali Haider";
+// var na&me = "Ali Haider";
+// var 1name = "Ali Haider";
+var FullName = "Ali Haider";
+// var fullName = "Ali Haider       my name
+// dfskjhjgkdfh";
+// var full_name = 'Ali Haider         my name
+
+
+// dfskjhjgkdfh';
+var full_name = `Ali Haider`;
+// var full_name = `Ali Haider       my name 
+
+
+
+
+// dfskjhjgkdfh
+
+
+// fdjkhgdfjkhgf
+// jhfjdkhgjkdfh
+
+
+
+// jkdfhsd`;
+console.log("window", window);
+
+// console.log(full_name, "full_name");
+// var fullName = "Ali Haider"; // variable declaration and initialization
+// var fullName; // variable declaration
+// fullName = "Ali Haider"; // variable initialization
+// age = 25;
+// // var age = 25;
+// console.log(isStudent, "isStudent"); // hoisting example
+
+// var isStudent = true;
+// // console.log(window.name, "name");
+// // console.log(name, "name");
+
+// var name = "hasnain";
+
+// var isAdult;
+// console.log(isAdult, "isAdult before initialization");
+
+// if (age > 18) {
+//     isAdult = true;
+// } else {
+//     isAdult = false;
+// }
+// console.log(isAdult, "isAdult after initialization");
+// console.log(a, "is the sum of 10 + 20 + 30 + 40 + 50");
+// console.log("a:", a);
+// console.log("a:" + a);
+
+// // console.log("ali" + 10);
+
+// console.log(a, "a");
+// console.log(a, "a");
+// console.log(a, "a");
+// console.log(a, "a");
+// console.log(a, "a");
+// console.log(a, "a");
+// console.log(a, "a");
+// console.log(a, "a");
+
+// console.log(10 + 20 + 30 + 40 + 50);
+// console.log(10 + 20 + 30 + 40 + 50);
+// console.log(10 + 20 + 30 + 40 + 50);
+// console.log(10 + 20 + 30 + 40 + 50);
+// console.log(10 + 20 + 30 + 40 + 50);
+// console.log(10 + 20 + 30 + 40 + 50);
+// console.log(10 + 20 + 30 + 40 + 50);
+// console.log(10 + 20 + 30 + 40 + 50);
+
+// // IIFE
+// (function () {
+//     var message = "This is an IIFE example";
+//     console.log(message);
+// })()
+console.log(a, "a");
+
+var a = 10;
+// var a = 30;
+// console.log(a, "a");
+// console.log(a, "a");
+// a = 20;
+// console.log(a, "a");
+// console.log(city, "city");
+
+// let city = "Karachi";
+// // let city = "Karachi";
+// console.log("city", city);
+// city = "Lahore";
+// console.log("city", city);
+
+console.log(country, "country");
+const country = "Pakistan";
+// const country = "Pakistan";
+country = "India"; // This will cause an error
+// console.log(city, "city");
+console.log(country, "country");
+
+// console.log("hello");
+
