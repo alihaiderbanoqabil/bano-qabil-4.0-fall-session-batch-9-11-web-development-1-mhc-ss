77293284a4cb74132c7d395fcd01c06c5bb21d20
alihaiderbanoqabil <alihaider.banoqabil2.0@gmail.com>
2026-01-03 14:08:06 +0500

    done with class 3 of js

---
+++ class15/script.js
@@ -0,0 +1,283 @@
+console.log("Class 3 of JS");
+
+
+// // let studentName1 = "ali";
+// // let studentName2 = "ahmed";
+// // let studentName3 = "sara";
+// // let studentName4 = "lina";
+// // let studentName5 = "omar";
+// // let studentName6 = "nora";
+// // let studentName7 = "yousef";
+// // let studentName8 = "mona";
+// // let studentName9 = "khaled";
+// // let studentName10 = "dina";
+
+// // let studentNames = [
+// //     "ali",
+// //     "ahmed",
+// //     "sara",
+// //     "lina",
+// //     "omar",
+// //     "nora",
+// //     "yousef",
+// //     "mona",
+// //     "khaled",
+// //     "dina"
+// // ];
+
+// // console.log(studentNames, "studentNames before update");
+// // studentNames[1] = "hasnain";
+// // studentNames[1] = 80;
+// // studentNames[2] = { name: "omar", age: 20 };
+
+// // delete studentNames[4];
+
+// // console.log(studentNames, "studentNames after update");
+
+// // const studentMarks = [85, 92, 78, 90, 88, 76, 95, 89, 84, 91];
+
+// // const studentAges = [20, 21, 19, 22, 20, 23, 21, 22, 20, 19];
+
+// // const isStudentPassed = [true, true, false, true, true, false, true, true, true, true];
+
+// // const mixedArray = [
+// //     "usman",
+// //     22,
+// //     false,
+// //     "ali",
+// //     85,
+// //     true,
+// //     "sara",
+// //     78,
+// //     false,
+// //     undefined,
+// //     "mona",
+// //     89,
+// //     true,
+// //     null,
+// //     { name: "omar", age: 20 },
+// //     "testing",
+// //     [1, 2, 3]
+// // ];
+
+
+// // console.log(mixedArray, "mixedArray");
+// // console.log(mixedArray[11], "mixedArray[11]");
+// // console.log(mixedArray.length, "mixedArray.length");
+// // console.log(mixedArray[mixedArray.length - 1], "mixedArray[12]");
+
+// // console.log(studentNames[4], "studentNames[4]");
+// // console.log(studentNames[7], "studentNames[7]");
+
+
+// // console.log(studentNames, "studentNames");
+
+
+// let student1Name = "usman";
+// let student1Age = 22;
+// let student1City = "karachi";
+// let student1IsPassed = true;
+// let student1Marks = [85, 92, 78, 90, 88];
+
+
+// let student2Name = "usmandsfdfs";
+// let student2Age = 223453;
+// let student2City = "karadsfsdchi";
+// let student2IsPassed = true;
+// let student2Marks = [85, 91, 78, 90, 88];
+
+
+// let studentInformation = {
+//     name: "usman",
+//     age: 22,
+//     age: 27,
+//     city: "karachi",
+//     "is Passed": true,
+//     "is Passed": false,
+//     // isPassed: false,
+//     marks: [85, 92, 78, 90, 88],
+//     hobbies: ["reading", "coding", "gaming"],
+//     address: {
+//         street: "123 Main St",
+//         zipCode: "75270",
+//         country: "Pakistan"
+//     }
+// };
+
+
+
+// let studentsInformation = [
+//     {
+//         name: "usman",
+//         age: 22,
+//         age: 27,
+//         city: "karachi",
+//         "is Passed": true,
+//         "is Passed": false,
+//         // isPassed: false,
+//         marks: [85, 92, 78, 90, 88],
+//         hobbies: ["reading", "coding", "gaming"],
+//         address: {
+//             street: "123 Main St",
+//             zipCode: "75270",
+//             country: "Pakistan"
+//         }
+//     },
+//     {
+//         name: "ali",
+//         age: 23,
+//         city: "lahore",
+//         isPassed: true,
+//         marks: [75, 82, 88, 80, 78],
+//         hobbies: ["sports", "music"],
+//         address: {
+//             street: "456 Market St",
+//             zipCode: "54000",
+//             country: "Pakistan"
+//         }
+//     }
+// ];
+
+// console.log(studentInformation.city, "studentInformation.city");
+// console.log(studentInformation.address.country, "studentInformation.address.country");
+// console.log(studentInformation.hobbies, "studentInformation.hobbies");
+// console.log(studentInformation.hobbies[2], "studentInformation.hobbies[2]");
+
+// // console.log(studentInformation.is Passed, "studentInformation");
+// console.log(studentInformation["is Passed"], "studentInformation");
+// console.log(studentInformation["age"], "studentInformation");
+
+
+// const product = {
+//     "id": 676,
+//     "name": "HK10 Ultra 3 2.02 AMOLED Display G-Sensor AI Features With WearFit Pro App 2 Straps - Silver",
+//     "slug": "hk10-ultra-3-2-02-amoled-display-g-sensor-ai-features-with-wearfit-pro-app-2-straps-silver",
+//     "sku": "SKU-20251205-JXSADH",
+//     "barcode": "HK10 Ultra 3 2.02",
+//     "model": null,
+//     "featured_image": "https://amanoon-bucket.s3.me-central-1.amazonaws.com/amanoon_pk_live/products/featured/216w5zfEIuEHOMlSuHGe9aI40FG4zvlDeUgetdQh.jpg",
+//     "short_description": "<p><strong>Description</strong>: HK10 ULTRA 3 2.02 AMOLED DISPLAY G-SENSOR AI FEATURES WITH WEARFIT PRO APP 2 STRAPS SILVER -</p><p><strong>Detail</strong>: Smart Watch Specifications and Features</p><p><strong>Technical Parameters</strong></p><p><strong>Screen Size</strong>: 2.02-inch full AMOLED screen</p><p><strong>Resolution</strong>: 410×494 dpi</p><p><strong>Features</strong>: Full-screen, vibrant display</p><p><strong>Hardware</strong>:</p><p><strong>Chip</strong>:&nbsp;<strong>Newest SF32LB525</strong>&nbsp;with dual-core architecture, integrated 3D graphics engine, and AI neural network accelerator</p><p><strong>CPU</strong>: 1.4 MB running memory + 1 GB storage memory</p><p><strong>Speaker</strong>: Upgraded 1508 high-power system</p><p style=\"text-align: left;\"><strong>Sensors Pedometer</strong>: SC7A20H</p><p style=\"text-align: left;\"><strong>Heart Rate</strong>: 1662</p><p style=\"text-align: left;\"><strong>G-Sensor</strong>: Gravity-sensing dials</p><p style=\"text-align: left;\"><strong>Connectivity:</strong></p><p style=\"text-align: left;\"><strong>Bluetooth</strong>:&nbsp;Version 5.3&nbsp;</p><p style=\"text-align: left;\">Compatibility:&nbsp;<strong>Android 5.0+ &amp; iOS 10.0+</strong></p><p style=\"text-align: left;\"><strong>App Support</strong>:&nbsp;WearfitPro</p><p style=\"text-align: left;\"><strong>Dimensions &amp; Power</strong>:</p><p style=\"text-align: left;\"><strong>Size</strong>: 49×44×12 mm</p><p style=\"text-align: left;\"><strong>Battery</strong>: 260 mAh high-voltage, magnetic wireless charging</p><p style=\"text-align: left;\"><strong>Waterproof</strong>: Life waterproof (handwashing and light rain; not for swimming or bathing)</p><p style=\"text-align: left;\"><strong>Fitness &amp; Activity:</strong><br>Sports mode and detailed activity recording<br>Step counter and real compass</p><p style=\"text-align: left;\"><strong>Convenience Tools:</strong><br>Bluetooth calls, weather updates, alarms, and stopwatch<br>Timer, find phone, and screen-off customization<br>NFC access for IC door cards (13.56 MHz frequency; supports ~80% of systems)</p><p style=\"text-align: left;\"><strong>NFC Access</strong>:<br>Functions as an IC door card for frequencies of 13.56 MHz only.<br>Requires setup via the access control system or property management office.</p><p style=\"text-align: left;\"><strong>Charging Guidelines</strong>:</p><p style=\"text-align: left;\">Use the original&nbsp;<strong>HK10 Ultra 3 charging cable</strong>.<br>Compatible power adapter:&nbsp;<strong>5V/1A (5W max)</strong>.</p><p style=\"text-align: left;\"><strong>Waterproofing</strong>:<br>Designed for daily water exposure (handwashing, rain).<br>Avoid submersion, swimming, or exposure to corrosive liquids.</p><p style=\"text-align: left;\"><strong>Sensor Indicators</strong>:<br>Normal operation: 3 sensor lights active.</p>",
+//     "description": "<p></p>",
+//     "is_active": 1,
+//     "fulfilled": 1,
+//     "is_featured": 1,
+//     "product_type": "simple",
+//     "condition": "new",
+//     "length": 0,
+//     "width": 0,
+//     "height": 0,
+//     "weight": 0,
+//     "delivery_fees": null,
+//     "tax_id": 1,
+//     "parent_id": null,
+//     "brand_id": 64,
+//     "created_by_id": 4,
+//     "updated_by_id": 4,
+//     "meta_title": "Buy HK10 Ultra 3 2.02 AMOLED Display Smartwatch Silver Price in Pakistan",
+//     "meta_description": "Buy the HK10 Ultra 3 smartwatch Silver — 2.02″ AMOLED, sleek design & packed features. Check latest price in Pakistan and order today!",
+//     "meta_keywords": "HK10 Ultra 3 2.02 AMOLED Display Smartwatch Silver Price in Pakistan, HK10 Ultra 3 2.02 AMOLED Display Smartwatch Silver Price in Karachi, HK10 Ultra 3 2.02 AMOLED Display Smartwatch Silver Price in Lahore, HK10 Ultra 3 2.02 AMOLED Display Smartwatch Silver Price in Islamabad, HK10 Ultra 3 2.02 AMOLED Display Smartwatch Silver Price in Peshawar",
+//     "created_at": "2025-12-05T10:29:59.000000Z",
+//     "updated_at": "2025-12-05T10:32:53.000000Z",
+//     "price": 6250,
+//     "sale_price": null,
+//     "final_price": 6250,
+//     "discount_percentage": 0,
+//     "in_stock": true,
+//     "seller": "Kharedlo.pk",
+//     "seller_slug": "kharedlopk",
+//     "brand": {
+//         "id": 64,
+//         "name": "No Brand",
+//         "slug": "no-brand"
+//     },
+//     "categories": [
+//         {
+//             "id": 85,
+//             "name": "Wearable Technology",
+//             "slug": "smartwatches-and-accessories",
+//             "commission_rate": 0,
+//             "pivot": {
+//                 "product_id": 676,
+//                 "category_id": 85
+//             }
+//         },
+//         {
+//             "id": 86,
+//             "name": "Smartwatches",
+//             "slug": "smartwatches",
+//             "commission_rate": 0,
+//             "pivot": {
+//                 "product_id": 676,
+//                 "category_id": 86
+//             }
+//         }
+//     ],
+//     "inventories": [
+//         {
+//             "id": 330,
+//             "product_id": 676,
+//             "business_id": 8,
+//             "sku": "SKU-20251208-PBSH8Y",
+//             "price": 6250,
+//             "is_active": 1,
+//             "sale_price": 0,
+//             "sale_starts_at": null,
+//             "sale_ends_at": null,
+//             "stock": 1,
+//             "stock_on_hold": 0,
+//             "stock_threshold": 0,
+//             "warranty": "No Warranty",
+//             "processing_days": "Same Day",
+//             "offer_note": null,
+//             "created_at": "2025-12-08T11:50:30.000000Z",
+//             "updated_at": "2025-12-08T11:50:30.000000Z",
+//             "business": {
+//                 "id": 8,
+//                 "name": "Kharedlo.pk",
+//                 "slug": "kharedlopk",
+//                 "company_legal_name": "Kharedlo Ecommerce"
+//             }
+//         }
+//     ]
+// };
+
+// console.log(product, "product");
+// console.log(product.categories[1].pivot.category_id, "product.categories[1].pivot.category_id");
+// product.categories[1].pivot.category_id = 10000
+// delete product.categories[1].pivot.category_id;
+// console.log(product, "product");
+
+let age1 = 100;
+
+let age2 = age1;
+
+age2 = 200;
+
+
+console.log(age1, "age1");
+console.log(age2, "age2");
+
+
+let person1 = {
+    name: "usman",
+    age: 22,
+    address: {
+        city: "karachi",
+        country: "pakistan"
+    },
+    hobbies: ["reading", "coding"]
+};
+
+// let person2 = person1; // normal assignment
+let person2 = structuredClone(person1); // deep copy
+
+person2.name = "ali";
+person2.address.city = "lahore";
+person2.hobbies[0] = "gaming";
+
+console.log(person1, "person1");
+console.log(person2, "person2");
+
+
+
+
